d44d0055a955f9fadf22d19eaf829ad239c07f2b
cristianc3005-sketch <cristianterri3005@gmail.com>
2026-05-29 20:31:01 -0500

    Update Usuario.java

--- backend/sistema-gastos/src/main/java/com/gastos/hogar/Entidades/Usuario.java
+++ backend/sistema-gastos/src/main/java/com/gastos/hogar/Entidades/Usuario.java
@@ -1,6 +1,9 @@
 package com.gastos.hogar.Entidades;
 
 import jakarta.persistence.*;
+
+import jakarta.validation.constraints.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,10 +21,21 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    
+@NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, message = "El nombre debe tener mínimo 3 caracteres")
+
     private String nombre;
 
     @Column(unique = true)
+    
+@NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Correo inválido")
+
     private String correo;
+    
+@NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener mínimo 6 caracteres")
 
     private String password;
 
