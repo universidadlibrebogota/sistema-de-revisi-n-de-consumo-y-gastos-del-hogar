2496c0203753a06e8cffa7f7564119410f3fd729
cristianc3005-sketch <cristianterri3005@gmail.com>
2026-05-29 20:32:48 -0500

    Update UsuarioDTO.java

--- backend/sistema-gastos/src/main/java/com/gastos/hogar/DTO/UsuarioDTO.java
+++ backend/sistema-gastos/src/main/java/com/gastos/hogar/DTO/UsuarioDTO.java
@@ -5,8 +5,17 @@ import lombok.Data;
 @Data
 public class UsuarioDTO {
 
+
+  @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, message = "El nombre debe tener mínimo 3 caracteres")
     private String nombre;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Correo inválido")
     private String correo;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener mínimo 6 caracteres")
     private String password;
 
 }
